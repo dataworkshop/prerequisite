e912e02f0e1e034d30a8d12cbc172af1749889c7
Vladimir Alekseichenko <vova@vova.me>
2016-09-26 14:12:23 +0200

    add extra visual libraries

--- run.py
+++ run.py
@@ -5,13 +5,24 @@ import importlib
 
 packages = {
     "IPython": "4.0",
+    "ipywidgets": "4.1.1",
     "pandas": "0.18",
     "numpy": "1.10",
     "matplotlib": "1.5",
     "seaborn": "0.7",
     "sklearn": "0.17",
     "xgboost": "0.4",
-    "ggplot": "0.10"
+    "ggplot": "0.10",
+    "ml_metrics": "0.1.4",
+}
+
+additional_visual_packages = {
+    "gmplot": "1.1.1",
+    "geoplotlib": "0.3.2",
+    "folium": "0.2.1",
+    "vincent": "0.4.4",
+    "geopandas": "0.2.1",
+    "mpl_toolkits.basemap": "1.0.7"
 }
 
 def verify_packages(packages):
@@ -20,9 +31,12 @@ def verify_packages(packages):
 
     for package_name, package_version in packages.items():
         current_version = get_version_package(package_name)
+
         if False == current_version:
             print(colored.red("{0} - missing".format(package_name)))
             missing_packages.append(package_name)
+        elif current_version is None:
+            handle_package_without_version(current_version, package_version, package_name)
         else:
             if version_is_good(current_version, package_version):
                 print(colored.green("{0}-{1} - OK".format(package_name, current_version)))
@@ -32,18 +46,25 @@ def verify_packages(packages):
 
     return missing_packages, upgrade_packages
 
+def handle_package_without_version(current_version, package_version, package_name):
+    if current_version != package_version:
+        print(colored.yellow("{0} exists, but has no attribute version. Expected versin {1}".format(package_name, package_version)))
+    else:
+        print(colored.green("{0} - OK".format(package_name)))
+
 def get_version_package(package_name):
     try:
         return importlib.import_module(package_name).__version__
     except ImportError:
         return False
+    except AttributeError:
+        return None
 
 def version_is_good(actual_version, expected_version):
     return StrictVersion(actual_version) >= StrictVersion(expected_version)
 
 if __name__ == '__main__':
     missing_packages, upgrade_packages = verify_packages(packages)
-
 
     if not missing_packages and not upgrade_packages:
         print("")
